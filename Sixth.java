dd806202d229186020b88d897626564612686503
aigulmal <33079667+aigulmal@users.noreply.github.com>
2022-11-02 00:06:13 +0900

    Add files via upload
    upgrade

--- Sixth.java
+++ Sixth.java
@@ -1,53 +1,35 @@
 import java.util.Scanner;
 
 public class Sixth {
-    public static void main(String[] args){
-        //1.1
-        //int power=0;
-        //int result = 0;
-        /*
-        while (power<10){
-            result = (int) Math.pow(2, power);
-            power++;
+    void oneOne(){
+        int result = 1;
+        while (result<=512) {
             System.out.println(result);
-        }*/
-        //1.2
-        /*
-        do{
-            result = (int) Math.pow(2, power);
-            power++;
-            System.out.println(result);
+            result *= 2;
         }
-        while (power<10);
-        */
-        //1.1.1
-        /*int result = 1;
-        while (result<=512){
-            System.out.println(result);
-            result*=2;
-        }
-        */
-        //1.1.2
-        /*int result = 1;
+    }
+    void oneTwo(){
+        int result = 1;
         do {
             System.out.println(result);
             result*=2;
         } while (result<=512);
-        */
-        //2
-        /*
+    }
+    void Two() {
         int i = 0;
-        while(i < 50) {
+        System.out.println(i);
+        while (i < 50) {
             i++;
-            if (i == 4 || i==24){
+            if (i == 4 || i == 24) {
                 continue;
             }
-            if(i==48){
+            if (i == 48) {
                 break;
             }
             System.out.println(i);
-        }*/
-        //3
+        }
+    }
+    void Three(){
         System.out.println("Введите число n: ");
         Scanner in = new Scanner(System.in);
         int n;
@@ -57,5 +39,12 @@ public class Sixth {
             factorial*=i;
         }
         System.out.println("Факториал числа " +n +" равен "+  factorial);
+    }
+
+    public static void main(String[] args){
+        Sixth sixth = new Sixth();
+
+        sixth.Three();
+
     }
 }
